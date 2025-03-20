bafa2a6efbb3c59c902cb7cfa750f4158f9428f2
MykyBalb <mykaelbalbino06@gmail.com>
2025-03-20 17:28:22 -0300

    uu

--- Jpa3Classes/Jpa3Classes/src/main/java/com/example/Jpa3Classes/Controller/ControllerCurso.java
+++ Jpa3Classes/Jpa3Classes/src/main/java/com/example/Jpa3Classes/Controller/ControllerCurso.java
@@ -1,10 +1,9 @@
 package com.example.Jpa3Classes.Controller;
 
 import com.example.Jpa3Classes.Entity.Curso;
-import com.example.Jpa3Classes.Entity.Professor;
 import com.example.Jpa3Classes.Repository.CursoRepository;
+import org.apache.tomcat.util.http.parser.HttpParser;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -26,13 +25,36 @@ public class ControllerCurso {
     }
 
     @GetMapping
-    public ResponseEntity<Curso> getAll(@RequestBody Curso curso){
-        List<Curso> cursoOptional = cursoRepository.findAll();
-        if (cursoOptional.isEmpty()) {
-            return  ResponseEntity.status(HttpStatus.FOUND).body(curso);
+    public ResponseEntity<List<Curso>> getAll(){
+        List<Curso> cursoList = cursoRepository.findAll();
+        if (!cursoList.isEmpty()) {
+            return  ResponseEntity.status(HttpStatus.FOUND).body(cursoList);
         } else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+    @GetMapping("/{idCurso}")
+    public ResponseEntity<Optional<Curso>> getById(@PathVariable long idCurso){
+        Optional<Curso> curso = cursoRepository.findById(idCurso);
+        if(curso.isPresent()){
+            return ResponseEntity.status(HttpStatus.FOUND).body(curso);
+        }else {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+    }
+
+    @PutMapping("/{idCurso}")
+    public ResponseEntity<Optional<Curso>> update(@PathVariable long idCurso, @RequestBody Curso curso){
+        Optional<Curso> cursoOptional = cursoRepository.findById(idCurso);
+        if(cursoOptional.isPresent()){
+            curso.setNome(curso.getNome());
+            curso.setNumeroSala(curso.getNumeroSala());
+            curso.setProfessor(curso.getProfessor());
+            return ResponseEntity.status(HttpStatus.FOUND).body(cursoOptional);
+        }else{
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
     }
 
 }
+
