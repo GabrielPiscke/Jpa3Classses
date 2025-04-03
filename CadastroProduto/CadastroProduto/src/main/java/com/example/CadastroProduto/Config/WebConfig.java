dc7b38968b5e022ce8470472dc9aa4674254fe87
Biel <gabrielmateuspiscke@gmail.com>
2025-04-03 17:26:46 -0300

    upd3

--- CadastroProduto/CadastroProduto/src/main/java/com/example/CadastroProduto/Config/WebConfig.java
+++ CadastroProduto/CadastroProduto/src/main/java/com/example/CadastroProduto/Config/WebConfig.java
@@ -1,11 +1,11 @@
-package com.example.CadastroProduto.WebMVC;
+package com.example.CadastroProduto.Config;
 
 import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-
-@Configurable
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
